2c94fe142aaa2ddd1f5917aedd2168071f9173ca
yumin359 <yumin359@gmail.com>
2025-06-23 09:41:46 +0900

    0623 삭제후 alert

--- src/main/java/com/example/prj1/controller/BoardController.java
+++ src/main/java/com/example/prj1/controller/BoardController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -67,8 +69,12 @@ public class BoardController {
     // 남은 시간에는 꾸미는 거 한대용
 
     @PostMapping("remove")
-    public String remove(Integer id) {
+    public String remove(Integer id, RedirectAttributes redirectAttributes) {
         boardService.remove(id);
+
+        redirectAttributes.addFlashAttribute("alert",
+                Map.of("code", "danger", "message", id + "번 게시물이 삭제 되었습니다."));
+
         return "redirect:/board/list";
     }
 
